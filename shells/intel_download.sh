f06c84a9f4b5124fff13f1e6b983b7f40ee8b14b
Daniel Ng <ducduy.ababat@gmail.com>
2020-07-30 14:34:33 -0500

    intel dataset download script

---
+++ shells/intel_download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+input=$1
+
+while IFS= read -r line; do
+  for N in 1 2 3 4 5 6 7 8 9; do
+    python $(pwd)/runner.py -i "$line" -n "$N"
+  done
+
+  python $(pwd)/runner.py -i "$line" -n 10 -r
+done <"$input"
