61e24cd58df5e57e37e9253ca4b390fcfb0bad9b
Daniel Ng <ducduy.ababat@gmail.com>
2020-07-30 14:48:29 -0500

    yeah it fixed

--- shells/intel_download.sh
+++ shells/intel_download.sh
@@ -4,8 +4,8 @@ input=$1
 
 while IFS= read -r line; do
   for N in 1 2 3 4 5 6 7 8 9; do
-    python runner.py -i "$line" -n "$N"
+    python runner.py -i "$line" -n "$N" -q
   done
 
-  python runner.py -i "$line" -n 10 -r
+  python runner.py -i "$line" -n 10 -r -q
 done <"$input"
