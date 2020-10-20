a2c31f8a6f7d9f4f209d9d18d3e5bb6a15267178
Daniel Ng <ducduy.ababat@gmail.com>
2020-10-20 18:57:56 -0500

    interpreter

--- start2.sh
+++ start2.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 input=$1
+intepreter=$2
 
 if [ $UID != 0 ]; then
 
   while IFS= read -r line; do
     for N in {1..999}; do
-      sudo python3 multiprocess_runner.py -i "$line" -n "$N"
+      sudo $intepreter multiprocess_runner.py -i "$line" -n "$N"
     done
 
-    sudo python3 multiprocess_runner.py -i "$line" -n 1000
+    sudo $intepreter multiprocess_runner.py -i "$line" -n 1000
   done <"$input"
 fi
